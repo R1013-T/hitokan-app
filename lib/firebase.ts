4639094cc0bd8406eae8066eeb8ded01c8948d0a
R1013-T <nosuke1013@icloud.com>
2023-02-18 03:08:12 +0900

    done

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyAOwDPsSGnTsAFlvLelH5sc0hl5LcoFfgQ",
+  authDomain: "hitokan-app.firebaseapp.com",
+  projectId: "hitokan-app",
+  storageBucket: "hitokan-app.appspot.com",
+  messagingSenderId: "637945751202",
+  appId: "1:637945751202:web:cd0abe7b8b9c48f25d99fb"
 };
 
 const app = initializeApp(firebaseConfig);

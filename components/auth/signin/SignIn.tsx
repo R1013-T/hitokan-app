4ab26d3d348243f6d3a0968903510306b05ccca9
R1013-T <nosuke1013@icloud.com>
2023-02-18 17:12:25 +0900

    done

--- components/auth/signin/SignIn.tsx
+++ components/auth/signin/SignIn.tsx
@@ -12,9 +12,8 @@ interface Props {
 const Signin = (props: Props) => {
   const [isLoading, setIsLoading] = useState(false);
 
-
-  const changeIsLoading = (state:boolean) => {
-    setIsLoading(state)
+  const changeIsLoading = (state: boolean) => {
+    setIsLoading(state);
   };
 
   return (
@@ -26,7 +25,9 @@ const Signin = (props: Props) => {
       {isLoading ? <Loading text="ログイン中です" /> : ""}
       <p className={styles.title}>LOG IN TO YOUR ACCOUNT</p>
       <Input changeIsLoading={changeIsLoading} />
-      <button onClick={() => props.changeAuthState("signup")} >Don't have your account ?</button>
+      <button
+        onClick={() => props.changeAuthState("signup")}
+      >{`Don't have your account ?`}</button>
     </div>
   );
 };
